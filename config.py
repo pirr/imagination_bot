b7ec3445c2b152fef80f3aff7099328c48ce59d9
al_smaga <tattoo1mag@gmail.com>
2019-08-27 10:47:24 +0800

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Этот токен невалидный, можете даже не пробовать :)
-token = '399233874:AAGB3vtACdXnVLMzW_HQIMFuTZ9MF24aBG4'
-path_url = 'https://api.telegram.org/bot399233874:AAGB3vtACdXnVLMzW_HQIMFuTZ9MF24aBG4/'
